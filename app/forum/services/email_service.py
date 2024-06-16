2ed0ca8c9ae57a674e875ee4ccb2f36a4860fca3
Amanda Willrich Fuchs <amandawillrich@outlook.com>
2024-06-16 17:41:49 -0300

    Implementing email service

---
+++ app/forum/services/email_service.py
@@ -0,0 +1,34 @@
+import smtplib, ssl
+from email.message import EmailMessage
+from pathlib import os
+
+
+class EmailService:
+    
+    def __init__(self, port=None, smtp_server=None, sender_email=None, sender_password=None, receiver_email=None):
+        self.port = port or 465
+        self.smtp_server = smtp_server or 'smtp.gmail.com'
+        self.sender_email = sender_email or str(os.getenv('DEVELOPMENT_EMAIL'))
+        self.sender_password = sender_password or str(os.getenv('DEVELOPMENT_EMAIL_PASSWORD'))
+        self.receiver_email = receiver_email or str(os.getenv('DEVELOPMENT_EMAIL'))
+
+
+    def send(self, fullname, subject, email, message):
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(self.smtp_server, self.port, context=context) as server:
+            server.login(self.sender_email, self.sender_password)
+
+            server.sendmail(
+                self.sender_email,
+                self.receiver_email,
+                self.create_message_body(fullname, subject, email, message)
+            )
+
+    def create_message_body(self, fullname, subject, email, message):
+        return (f'''######################### FORMULÁRIO DE CONTATO #########################\n
+            Enviada por: {fullname}\n
+            Email: {email}\n
+            Assunto: {subject}\n\n
+            Mensagem: {message}\n
+            ######################### FIM FORMULÁRIO DE CONTATO #########################
+        ''').encode('utf-8', 'ignore')
--- app/forum/views/contact_view.py
+++ app/forum/views/contact_view.py
@@ -2,18 +2,33 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from forum.forms import ContactForm
+from forum.services import EmailService
 
 
 class ContactView(FormView):
     form_class = ContactForm
     success_url = reverse_lazy('contact')
     template_name = 'forum/contact_form.html'
-
+    email_service = EmailService()
 
     def form_valid(self, form):
-        messages.add_message(
-            self.request,
-            messages.SUCCESS,
-            'Enviado com sucesso!'
-        )
+        try:
+            self.email_service.send(
+                form.cleaned_data.get('fullname'),
+                form.cleaned_data.get('subject'),
+                form.cleaned_data.get('email'),
+                form.cleaned_data.get('message')
+            )
+        except Exception as e:
+            messages.add_message(
+                self.request,
+                messages.WARNING,
+                'Algo deu errado. Tente novamente mais tarde.'
+            )
+        else:
+            messages.add_message(
+                self.request,
+                messages.SUCCESS,
+                'Mensagem enviada com sucesso!'
+            )
         return super().form_valid(form)
