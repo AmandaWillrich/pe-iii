3a4410f647c5a4383e7ae9f6467925a3df53809d
Amanda Willrich Fuchs <amandawillrich@outlook.com>
2024-07-12 21:02:26 -0300

    adding login required mixin to topic detail view

--- app/forum/views/topic_detail_view.py
+++ app/forum/views/topic_detail_view.py
@@ -1,6 +1,17 @@
 from forum.models import Topic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
+from django.contrib import messages
 
 
-class TopicDetailView(DetailView):
+class TopicDetailView(LoginRequiredMixin, DetailView):
     model = Topic
+    login_url = 'login'
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(
+                self.request,
+                messages.WARNING,
+                'Para acessar o tópico, é necessário estar logado.'
+            )
+        return super().dispatch(request, *args, **kwargs)
