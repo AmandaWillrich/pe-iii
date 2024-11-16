e1fd1072c23556fd7d68045bf85a44c636cf8051
Amanda Willrich Fuchs <amandawillrich@outlook.com>
2024-11-16 11:54:51 -0300

    set default debug to false

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost', '10.0.2.2']
 
